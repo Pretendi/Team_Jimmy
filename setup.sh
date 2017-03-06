d03ce479570c93e12f57aa33a8bc2bec21e349e0
Omar Abdeldayem <omar.abdeldayem@outlook.com>
2017-03-06 18:51:23 -0500

    Fixed download script to prevent duplicate downloads

--- setup.sh
+++ setup.sh
@@ -10,21 +10,21 @@ TITLA_START=2016
 if [ $ALLDATA = true ]; then
     TITLA_START=2013
     for year in `seq 1953 2013`; do
-        for month in `seq 1 12`; do
-            for day in `seq 1 31`; do
+#        for month in `seq 1 12`; do
+ #           for day in `seq 1 31`; do
                 echo "Downloading TORONTO LESTER B. PEARSON INTL A, ${year}/${month}/${day}"
-                wget --content-disposition "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=5097&Year=${year}&Month=${month}&Day=${day}&timeframe=2&submit= Download+Data"
-            done
-        done
+                wget --content-disposition "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=5097&Year=${year}&Month=1&Day=1&timeframe=2&submit= Download+Data"
+ #           done
+ #       done
     done
 fi
 
 # Download data from the new YYZ station
 for year in `seq $TITLA_START 2017`; do
-    for month in `seq 1 12`; do
-        for day in `seq 1 31`; do
+  #  for month in `seq 1 12`; do
+   #     for day in `seq 1 31`; do
             echo "Downloading TORONTO INTL A, ${year}/${month}/${day}"
-            wget --content-disposition "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=51459&Year=${year}&Month=${month}&Day=${day}&timeframe=2&submit= Download+Data"
-        done
-    done
+            wget --content-disposition "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=51459&Year=${year}&Month=1&Day=1&timeframe=2&submit= Download+Data"
+#    done
 done
+
